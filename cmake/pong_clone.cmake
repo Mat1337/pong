4b9809d5442287a7983856c5a226630b6cd97a4d
Mat <matkovukovic25@gmail.com>
2022-06-10 19:49:33 +0200

    cleanup

--- cmake/pong_clone.cmake
+++ cmake/pong_clone.cmake
@@ -1,53 +1,8 @@
-#################### GLOBAL #####################
-
-# include the external project
-include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
-
-# set the vendor directory
-set(VENDOR_DIRECTORY "${CMAKE_HOME_DIRECTORY}/vendor")
-
 # setup the resources directory
 set(RESOURCES_DIRECTORY "${CMAKE_HOME_DIRECTORY}/res")
 
 # setup the sources directory
 set(SOURCES_DIRECTORY "${CMAKE_HOME_DIRECTORY}/src")
-
-################### FREETYPE #####################
-
-# set the freetype directory
-set(FREETYPE_DIRECTORY "${VENDOR_DIRECTORY}/freetype")
-
-# disable harfbuzz
-set(FT_DISABLE_HARFBUZZ ON CACHE BOOL "" FORCE)
-
-# add the freetype directory as a sub directory
-add_subdirectory(${FREETYPE_DIRECTORY})
-
-#################### GLFW #####################
-
-# find the opengl package
-find_package(OpenGL REQUIRED)
-
-# set the glfw directory
-set(GLFW_DIRECTORY "${VENDOR_DIRECTORY}/glfw")
-
-# disable the extra stuff that glfw builds
-set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-
-# add the glfw directory as a sub directory
-add_subdirectory(${GLFW_DIRECTORY})
-
-#################### GLAD #####################
-
-# set the flag directory
-set(GLAD_DIRECTORY "${VENDOR_DIRECTORY}/glad")
-
-# add the glad directory as a sub directory
-add_subdirectory(${GLAD_DIRECTORY})
-
-###############################################
 
 # macro that creates the application executable
 MACRO(ADD_APP name)
@@ -55,7 +10,7 @@ MACRO(ADD_APP name)
     file(GLOB_RECURSE SOURCE_LIST "${SOURCES_DIRECTORY}/*.c")
 
     # create the executable
-    add_executable(${name} "${SOURCE_LIST}")
+    add_executable(${name} ${SOURCE_LIST})
 
     # link all the libraries
     target_link_libraries(${name} freetype glfw glad OpenGL::GL)
---
+++ cmake/pong_clone_libraries.cmake
@@ -0,0 +1,39 @@
+# set the vendor directory
+set(VENDOR_DIRECTORY "${CMAKE_HOME_DIRECTORY}/vendor")
+
+################### FREETYPE #####################
+
+# set the freetype directory
+set(FREETYPE_DIRECTORY "${VENDOR_DIRECTORY}/freetype")
+
+# disable harfbuzz
+set(FT_DISABLE_HARFBUZZ ON CACHE BOOL "" FORCE)
+
+# add the freetype directory as a sub directory
+add_subdirectory(${FREETYPE_DIRECTORY})
+
+#################### GLFW #####################
+
+# find the opengl package
+find_package(OpenGL REQUIRED)
+
+# set the glfw directory
+set(GLFW_DIRECTORY "${VENDOR_DIRECTORY}/glfw")
+
+# disable the extra stuff that glfw builds
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+
+# add the glfw directory as a sub directory
+add_subdirectory(${GLFW_DIRECTORY})
+
+#################### GLAD #####################
+
+# set the flag directory
+set(GLAD_DIRECTORY "${VENDOR_DIRECTORY}/glad")
+
+# add the glad directory as a sub directory
+add_subdirectory(${GLAD_DIRECTORY})
+
+###############################################
